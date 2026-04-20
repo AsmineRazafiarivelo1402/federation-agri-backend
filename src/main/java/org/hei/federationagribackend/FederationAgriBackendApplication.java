6a4845b6e6c9da33d4f27113ef84338f0533f878
valeria <hei.valeria.2@gmail.com>
2026-04-20 22:17:38 +0300

    Initialize project

---
+++ src/main/java/org/hei/federationagribackend/FederationAgriBackendApplication.java
@@ -0,0 +1,13 @@
+package org.hei.federationagribackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FederationAgriBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FederationAgriBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/hei/federationagribackend/FederationAgriBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hei.federationagribackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FederationAgriBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
