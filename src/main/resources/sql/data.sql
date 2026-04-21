74855d8ae8e0bc3d4e7f84e8fad4d27a8923cf17
valeria <hei.valeria.2@gmail.com>
2026-04-21 23:07:39 +0300

    Add data in database

--- src/main/resources/sql/data.sql
+++ src/main/resources/sql/data.sql
@@ -1,40 +1,21 @@
-CREATE TYPE gender AS ENUM ('MALE', 'FEMALE');
-CREATE TYPE member_occupation AS ENUM ('JUNIOR', 'SENIOR', 'SECRETARY', 'TREASURER', 'VICE_PRESIDENT', 'PRESIDENT');
+INSERT INTO collectivity (id, location, federation_approval) VALUES
+                                                                 ('COLL-001', 'Antananarivo - Ambohimanarina', true),
+                                                                 ('COLL-002', 'Antsirabe - Vakinankaratra', true),
+                                                                 ('COLL-003', 'Fianarantsoa - Haute Matsiatra', false);
 
-CREATE TABLE collectivity (
-                              id VARCHAR(255) PRIMARY KEY,
-                              location VARCHAR(255) NOT NULL,
-                              federation_approval BOOLEAN DEFAULT FALSE NOT NULL
-);
+INSERT INTO member (
+    id, first_name, last_name, birth_date, gender,
+    address, profession, phone_number, email,
+    occupation, registration_fee_paid, membership_dues_paid, collectivity_id
+) VALUES
+      ('MEM-001', 'Jean', 'Rakoto', '1985-05-15', 'MALE', 'Lot IV G 20', 'Riziculteur', 261340000001, 'jean.rakoto@gmail.com', 'PRESIDENT', true, true, 'COLL-001'),
+      ('MEM-002', 'Marie', 'Rasoa', '1990-08-20', 'FEMALE', 'Bâtiment B2', 'Éleveuse', 261340000002, 'marie.rasoa@yahoo.fr', 'TREASURER', true, true, 'COLL-002');
 
-CREATE TABLE member (
-                        id VARCHAR(255) PRIMARY KEY,
-                        first_name VARCHAR(255) NOT NULL,
-                        last_name VARCHAR(255) NOT NULL,
-                        birth_date DATE NOT NULL,
-                        gender gender NOT NULL,
-                        address TEXT,
-                        profession VARCHAR(255),
-                        phone_number VARCHAR(50),
-                        email VARCHAR(255) UNIQUE,
-                        occupation member_occupation NOT NULL,
-                        registration_fee_paid BOOLEAN DEFAULT FALSE NOT NULL,
-                        membership_dues_paid BOOLEAN DEFAULT FALSE NOT NULL,
-                        collectivity_id VARCHAR(255) REFERENCES collectivity(id)
-);
+INSERT INTO member (id, first_name, last_name, birth_date, gender, occupation, registration_fee_paid, membership_dues_paid, collectivity_id)
+VALUES ('MEM-003', 'Soa', 'Lala', '1995-12-01', 'FEMALE', 'JUNIOR', true, true, 'COLL-001');
 
+INSERT INTO member_referees (member_id, referee_id) VALUES
+    ('MEM-003', 'MEM-001');
 
-CREATE TABLE member_referees (
-                                 member_id VARCHAR(255) REFERENCES member(id) ON DELETE CASCADE,
-                                 referee_id VARCHAR(255) REFERENCES member(id) ON DELETE CASCADE,
-                                 PRIMARY KEY (member_id, referee_id)
-);
-
-
-CREATE TABLE collectivity_structure (
-                                        collectivity_id VARCHAR(255) PRIMARY KEY REFERENCES collectivity(id) ON DELETE CASCADE,
-                                        president_id VARCHAR(255) REFERENCES member(id),
-                                        vice_president_id VARCHAR(255) REFERENCES member(id),
-                                        treasurer_id VARCHAR(255) REFERENCES member(id),
-                                        secretary_id VARCHAR(255) REFERENCES member(id)
-);
+INSERT INTO collectivity_structure (collectivity_id, president_id, treasurer_id)
+VALUES ('COLL-001', 'MEM-001', 'MEM-002');
