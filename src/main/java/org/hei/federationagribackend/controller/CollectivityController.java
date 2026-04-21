abc70e63945ee1f95af70bc076b52a925fc4dd27
Miriasme <johndoe@example.com>
2026-04-21 16:45:33 +0300

    create entity Collectivity and Member

---
+++ src/main/java/org/hei/federationagribackend/controller/CollectivityController.java
@@ -0,0 +1,13 @@
+package org.hei.federationagribackend.controller;
+
+import org.hei.federationagribackend.entity.CollectivityEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class CollectivityController {
+
+}
---
+++ src/main/java/org/hei/federationagribackend/entity/CollectivityStructure.java
@@ -0,0 +1,13 @@
+package org.hei.federationagribackend.entity;
+
+public class CollectivityStructure {
+    private MemberEntity president;
+    private MemberEntity vicePresident;
+    private MemberEntity treasurer;
+    private MemberEntity secretary;
+
+    private String presidentId;
+    private String vicePresidentId;
+    private String treasurerId;
+    private String secretaryId;
+}
